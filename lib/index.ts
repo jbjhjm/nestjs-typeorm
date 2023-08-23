331b12378f295f60ccc928bc13fa2821f880cdec
Jannik Mewes <mewes@yooniversity.com>
2023-08-23 10:42:20 +0200

    export all stuff for better accessibility

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
 export * from './common';
 export * from './interfaces';
+export * from './typeorm-core.module';
+export * from './typeorm.constants';
+export * from './typeorm.providers';
 export * from './typeorm.module';
